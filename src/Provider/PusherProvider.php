bafde8146e6b78fd4843483dd830e357c1e9ab10
aolee <l33.adrian@gmail.com>
2025-10-28 23:51:22 +0800

    Update PusherProvider.php
    Inject Soketi host/port/scheme if configured in config.php

--- src/Provider/PusherProvider.php
+++ src/Provider/PusherProvider.php
@@ -12,6 +12,7 @@ namespace Flarum\Pusher\Provider;
 use Flarum\Foundation\AbstractServiceProvider;
 use Flarum\Settings\SettingsRepositoryInterface;
 use Pusher\Pusher;
+use Flarum\Pusher\SoketiConfig;
 
 class PusherProvider extends AbstractServiceProvider
 {
@@ -24,6 +25,18 @@ class PusherProvider extends AbstractServiceProvider
 
             if ($cluster = $settings->get('flarum-pusher.app_cluster')) {
                 $options['cluster'] = $cluster;
+            }
+
+            // Inject Soketi host/port/scheme if configured in config.php
+            $soketi = $this->container->make(SoketiConfig::class);
+            $host = $soketi->host();
+            $port = $soketi->port();
+            $tls  = $soketi->tls();
+
+            if ($host) {
+                $options['host']   = $host;
+                $options['port']   = $port ?? ($tls ? 443 : 6001);
+                $options['scheme'] = $tls ? 'https' : 'http';
             }
 
             return new Pusher(
